ceb183c2d65255eec46ad4172f8d9d98d30f55d4
sonam wangmo <osonam978@gmail.com>
2025-05-14 14:55:36 +0900

    fix

--- src/server.js
+++ src/server.js
@@ -17,4 +17,4 @@ app.use("/api/playlists", playlistRoutes);
 app.use("/api/songs", songRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
